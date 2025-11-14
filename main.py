dc02b9d770d9b985372ff43f9da64df1ec235dcf
Jacky Fang <jackyfang848@gmail.com>
2025-11-13 19:32:00 -0600

    Se distribuye el codigo en separados .py, teniendo main.py y puntajes.py, proximamente con jugar.py tendremos la logica de los modos de juego.

---
+++ main.py
@@ -0,0 +1,43 @@
+import tkinter as tk
+from tkinter import *
+import random
+from PIL import ImageTk, Image
+import pygame
+import puntajes
+
+
+window = tk.Tk()
+window.title("Escapa/Cazador")
+window.geometry("500x500")
+window.configure(bg="green")
+window.resizable(width=False, height=False)
+
+textoPrincipal = Label(text="Escapa del Laberinto", font=("Impact", 20), fg="Black", bg="green")
+textoPrincipal.pack()
+
+elegir = Label( text= "Elija un modo de juego",font=("Impact", 15), fg="Black", bg="green")
+elegir.place(x= 60, y= 150)
+
+boton_escapa = Button(text="Escapar")
+boton_escapa.place(x=60,y= 200)
+
+boton_cazar = Button(text="Cazador")
+boton_cazar.place(x= 160,y=200)
+
+puntos = Label(text="Tabla de puntajes",font=("Impact", 15), fg="Black", bg="green")
+puntos.place(x= 300,y=150)
+
+boton_puntos = Button(text="Puntajes", command=lambda: puntajes.abrir_puntaje(window))
+boton_puntos.place(x=340, y=200)
+
+nombre= Label(text="Ingrese su nombre",font=("Impact", 17), fg="Black", bg="green")
+nombre.pack()
+
+entry1 = Entry()
+entry1.config(font=("elephant",12), bg="Gray", fg="red", width=15)
+entry1.place(x=157, y=70)
+
+
+window.mainloop()
+
+
---
+++ puntajes.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+
+def abrir_puntaje(window):
+    puntaje = tk.Toplevel()
+    puntaje.title("Puntajes")
+    puntaje.geometry("350x350")
+    puntaje.resizable(width=False, height=False)
+    puntaje.configure(bg="green")
+
+    label_titulo = tk.Label(puntaje, text="Top 3 Puntajes.", font=("Impact", 15), bg="green")
+    label_titulo.place(x=110, y=40)
+
+
+
+    def cerrar_ventana():
+        puntaje.destroy()
+        window.deiconify()
+
+    boton_salir = tk.Button(puntaje, text="Salir", command=cerrar_ventana)
+    boton_salir.place(x=160, y=250)
+
+    window.withdraw()
